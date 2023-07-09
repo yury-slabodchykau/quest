587a90592efcd0e0b1b4ca68f724cdaa0986e399
Yury Slabodchykau <yury.slabodchykau@gmail.com>
2023-07-09 12:50:59 +0200

    Add __str__ method

--- main.py
+++ main.py
@@ -14,15 +14,23 @@ class Quest:
         if self.end_time:
             return 'С этим испытанием вы уже справились.'
         self.start_time = datetime.now()
-        return f'Начало "{self.name}" положено.'
+        return f'Начало квеста "{self.name}" положено.'
 
     def pass_quest(self):
-        if not self.start_time:
+        if self.start_time is None:
             return 'Нельзя завершить то, что не имеет начала!'
         self.end_time = datetime.now()
         completion_time = self.end_time - self.start_time
         return (f'Квест "{self.name}" окончен. '
                 f'Время выполнения квеста: {completion_time}')
+
+    def __str__(self):
+        result = f'Цель квеста {self.name} - {self.goal} '
+        if self.start_time and self.end_time:
+            result += 'Квест завершён.'
+        elif self.start_time:
+            result += 'Квест выполняется.'
+        return result
 
 
 quest_name = 'Сбор пиксельники'
@@ -39,3 +47,4 @@ print(new_quest.accept_quest())
 time.sleep(3)
 print(new_quest.pass_quest())
 print(new_quest.accept_quest())
+print(new_quest)
